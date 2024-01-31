480256517d6b891b3bb45c78aeebb1f985858981
Irina D <RabotaYd2021@yandex.ru>
2024-01-31 13:04:41 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int firstAmount = 500; // Начальный счет
+        int firstAmount = 500;  // Начальный счет
         int newAmount = 1500;   // Сумма пополнения
-        int totalAmount;    // Итоговый счет
+        int totalAmount;        // Итоговый счет
 
         int bonus;
-        if (newAmount > 1000){
+        if (newAmount > 1000) {
             bonus = newAmount / 100;
         } else {
             bonus = 0;
@@ -17,5 +17,5 @@ public class Main {
         System.out.println("Сумма пополнения: " + newAmount);
         System.out.println("Итоговый счет: " + totalAmount);
         System.out.println("Сумма бонусов: " + bonus);
-        }
     }
+}
